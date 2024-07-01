bf4db85f1ea8071bf2505da52c7966684758a902
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2024-07-01 09:57:21 -0600

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GameOfNim
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
