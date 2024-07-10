c569573c7ef26988ae553af8348f20bc41103029
BadAtRainbow6 <118947416+BadAtRainbow6@users.noreply.github.com>
2024-07-10 08:15:55 -0600

    Base Setup
    Setup swap and finalized player class

--- GameManager.cs
+++ GameManager.cs
@@ -10,9 +10,13 @@ namespace GameOfNim
     {
         public Player PlayerOne { get; set; }
         public Player PlayerTwo { get; set; }
-        public Player CurrentPlayer { get; set; }
+        public bool IsPlayer1 { get; set; } = true;
 
-        public GameManager() { }
+        public GameManager(string p1Name, string p2Name) 
+        {
+            PlayerOne = new Player(p1Name);
+            PlayerTwo = new Player(p2Name);
+        }
 
         public void Start()
         {
@@ -32,6 +36,11 @@ namespace GameOfNim
         private void Hard()
         {
             // number of lists
+        }
+
+        private void SwapPlayer()
+        {
+            IsPlayer1 = !IsPlayer1;
         }
 
         private bool CheckWin()
