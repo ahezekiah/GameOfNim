59492ca63855ba70d70348633497a7c5aada16ba
tjohnson2718 <tjohnson@student.neumont.edu>
2024-07-10 08:06:21 -0600

    Added GameManager and Player classes

---
+++ GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfNim
+{
+    public class GameManager
+    {
+        public Player PlayerOne { get; set; }
+        public Player PlayerTwo { get; set; }
+        public Player CurrentPlayer { get; set; }
+
+        public GameManager() { }
+
+        public void Start()
+        {
+            // number of lists
+        }
+
+        private void Easy()
+        {
+            // number of lists
+        }
+
+        private void Medium()
+        {
+            // number of lists
+        }
+
+        private void Hard()
+        {
+            // number of lists
+        }
+
+        private bool CheckWin()
+        {
+            return false;
+        }
+    }
+}
