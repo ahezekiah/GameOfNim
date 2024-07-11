086bc0285e84b1d8ede3ed71f895a7127594822c
Amanda Hezekiah <amanda.hezekiah@gmail.com>
2024-07-11 08:05:16 -0600

    These are the wins screens for both players

---
+++ WinScreenP2Hard.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GameOfNim
+{
+    /// <summary>
+    /// Interaction logic for WinScreenP2Hard.xaml
+    /// </summary>
+    public partial class WinScreenP2Hard : Window
+    {
+        public WinScreenP2Hard()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WinScreenP2Medium.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GameOfNim
+{
+    /// <summary>
+    /// Interaction logic for WinScreenP2Medium.xaml
+    /// </summary>
+    public partial class WinScreenP2Medium : Window
+    {
+        public WinScreenP2Medium()
+        {
+            InitializeComponent();
+        }
+    }
+}
